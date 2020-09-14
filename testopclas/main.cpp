4729d4697751cf7d6ae7b88d9b292e196e7b74f8
Aglorios17 <alessio.glorioso17@gmail.com>
2020-09-14 21:44:38 +0200

    classdebut

---
+++ testopclas/main.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include "class.h"
+#include <iostream>
+
+int main()
+{
+	Personnage david, goliath;
+
+	goliath.attaquer(david);
+	david.boirePotionDeVie(20);
+	goliath.attaquer(david);
+	david.attaquer(goliath);
+
+	goliath.changerArme("Double hache tranchante veneneuse de la mort", 40);
+	goliath.attaquer(david);
+
+	return (0);
+}
