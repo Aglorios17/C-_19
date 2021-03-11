bdc27585d04ab4ed054b37ed9423c38a548540e2
Aglorios17 <alessio.glorioso17@gmail.com>
2021-03-11 19:26:17 +0100

    cpp03/ex00

---
+++ Module_03/ex00/FragTrap.cpp
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aglorios <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/11 14:11:42 by aglorios          #+#    #+#             */
+/*   Updated: 2021/03/11 19:23:51 by aglorios         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap(void){}
+
+FragTrap::FragTrap(std::string name)
+{
+	this->Name = name;
+	this->Hit_points = 100;
+	this->Max_hit_points = 100;
+	this->Energy_points = 100;
+	this->Max_energy_points = 100;
+	this->Level = 1;
+	this->Melee_attack_damage = 50;
+	this->Ranged_attack_damage = 20;
+	this->Armor_damage_reduction = 5;
+	std::cout << this->Name << " appeared" << std::endl;
+}
+
+FragTrap::~FragTrap(void)
+{
+	if (this->Hit_points > 0)
+		std::cout << this->Name << " left" << std::endl;
+	else
+		std::cout << this->Name << " is dead" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap &copy){}
+
+void	FragTrap::rangedAttack(std::string const & target)
+{
+	std::cout << "FR4G-TP " << this->Name << " attaque " << target << " a distance, causant " << this->Ranged_attack_damage << " points de degats !" << std::endl; 
+}
+
+void	FragTrap::meleeAttack(std::string const & target)
+{
+	std::cout << "FR4G-TP " << this->Name << " attaque " << target << " par melee, causant " << this->Melee_attack_damage << " points de degats !" <<std::endl;
+}
+
+void	FragTrap::takeDamage(unsigned int amount)
+{
+	if (amount > this->Armor_damage_reduction)
+		amount -= this->Armor_damage_reduction;
+	else
+		amount = 0;
+	std::cout << this->Name << " perd " << amount << " points de degats ! (armor reduction : " << this->Armor_damage_reduction << " )." << std::endl;
+	if (this->Hit_points > amount)
+		this->Hit_points -= amount;
+	else
+		this->Hit_points = 0;
+	std::cout << this->Name << " a maintenant " << this->Hit_points << " points de vie !" << std::endl;
+}
+
+void	FragTrap::beRepaired(unsigned int amount)
+{
+	std::cout << this->Name << " gagne " << amount << " points de vie !" << std::endl;
+	if ((amount + this->Hit_points) < this->Max_hit_points)
+		this->Hit_points += amount;
+	else
+		this->Hit_points = 100;
+	std::cout << this->Name << " a maintenant " << this->Hit_points << " points de vie !" << std::endl;
+}
+
+void	FragTrap::beRepaired_energy(unsigned int amount)
+{
+	std::cout << this->Name << " gagne " << amount << " points d'energy !" << std::endl;
+	if ((amount + this->Energy_points) < this->Max_energy_points)
+		this->Energy_points += amount;
+	else
+		this->Energy_points = 100;
+	std::cout << this->Name << " a maintenant " << this->Energy_points << " points d'energy !" << std::endl;
+}
+
+std::string	FragTrap::get(void)
+{
+	return (this->Name);
+}
+
+int	FragTrap::get(std::string attack)
+{
+	if (attack == "Melee")
+		return (this->Melee_attack_damage);
+	if (attack == "Ranged")
+		return (this->Ranged_attack_damage);
+	return (0);
+}
+
+int	FragTrap::vaulthunter_dot_exe(std::string const & target)
+{
+	if (this->Energy_points > 24)
+	{
+		std::string attack[] = {"Par mal de sa mere", "Coup de coude dans le ventre", "Maudit les 5 prochaines generations", "Triple brulure indienne", "Petite baleyette"};
+		srand(time(0));
+		int i = rand() % 5;
+		this->Energy_points -= 25;	
+		std::cout << this->Name << " attaque " << target <<  " avec '" << attack[i] << "' et inflige " << attack[i].size() << " points de degats ! Il perd 25 d'energie actuellement : " << this->Energy_points << std::endl;
+		return (attack[i].size());
+	}
+	else
+		std::cout << this->Name << " a besoin de 25 points d'energie, actuellement : " << this->Energy_points << " points !" << std::endl;
+	return (0);
+}
+
+void	FragTrap::show(void)
+{
+	std::cout << "Name : " << this->Name << std::endl;
+	std::cout << "Live : " << this->Hit_points << std::endl;
+	std::cout << "Max Live : " << this->Max_hit_points << std::endl;
+	std::cout << "Energy : " << this->Energy_points << std::endl;
+	std::cout << "Max Energy : " << this->Max_energy_points << std::endl;
+	std::cout << "Level : " << this->Level << std::endl;
+	std::cout << "Melee attack : " << this->Melee_attack_damage << std::endl;
+	std::cout << "Ranged attack : " << this->Ranged_attack_damage << std::endl;
+	std::cout << "Armor damage reduction : " << this->Armor_damage_reduction << std::endl;
+}
---
+++ Module_03/ex00/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aglorios <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/11 14:11:58 by aglorios          #+#    #+#             */
+/*   Updated: 2021/03/11 19:23:55 by aglorios         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+int main()
+{
+	system("clear");
+	std::cout << "//////////////// INIT ///////////" << std::endl;
+	FragTrap a("Alessio");
+	FragTrap b("Elias");
+	std::cout << "///////////////// 1 /////////////" << std::endl;
+	a.rangedAttack(b.get());
+	b.takeDamage(a.get("Ranged"));
+	std::cout << "///////////////// 2 /////////////" << std::endl;
+	b.beRepaired(5);
+	std::cout << "///////////////// 3 /////////////" << std::endl;
+	a.meleeAttack(b.get());
+	b.takeDamage(a.get("Melee"));
+	std::cout << "///////////////// 4 /////////////" << std::endl;
+	b.meleeAttack(b.get());
+	a.takeDamage(a.get("Melee"));
+	std::cout << "///////////////// 5 /////////////" << std::endl;
+	int i = a.vaulthunter_dot_exe(b.get());
+	b.takeDamage(i);
+	std::cout << "///////////////// 6 /////////////" << std::endl;
+	a.meleeAttack(b.get());
+	b.takeDamage(a.get("Melee"));
+	std::cout << "///////////////// A /////////////" << std::endl;
+	a.show();
+	std::cout << "///////////////// B /////////////" << std::endl;
+	b.show();
+	std::cout << "//////////////// END ////////////" << std::endl;
+	return (1);
+}
